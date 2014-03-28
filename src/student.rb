d14f75e80e4f123cb42b7ed8890061a51116527a
YosukeOno <yyosuke.ono@gmail.com>
2014-03-28 16:22:05 +0900

    Replace File student.rb

--- src/student.rb
+++ src/student.rb
@@ -1,40 +1,52 @@
-#Make Student Class
+#Create Student Class
 class Student
-  #Initialize Student Class
+  #Initialize StudentClass
   def initialize( name, age)
     @name = name
     @age = age
   end
 
-  #Get name
-  def name
-    @name
-  end
-
-  #Get age
-  def age
-    @age
-  end
-
-  #Set name
-  def name=(value)
-    @name = value
-  end
-
-  #Set name
-  def age=(value)
-    @age = value
-  end
-
-  # Return String
+  #Return a String of instance of the StudentClass
   def to_s
     "#@name, #@age"
   end
 end
 
-#Generate Student Instance
-person = Student.new('name', #number)
+#Create Class of StudentBook Application
+class StudentBook
+  def initialize
+    @students = {}
+  end
 
-#Show Name Age
-puts "name:#{person.name}, age:#{person.age}"
+  #Create Multiple Instance of the Student Class to Hash
+  def setUpStudents
+    @students = {
+      :koba => Student.new("Mako Kobayashi", 32),
+      :fuzi => Student.new("Nao Fuzishima", 31),
+      :mori => Student.new("Yu Moriya", 23),
+    }
+  end
+
+  #Show All Instance a name and age
+  def printStudents
+    @students.each { |key, value|
+      puts "#{key}, #{value.to_s}"
+    }
+  end
+
+  #StudentBook to List the Date
+  def listAllStudents
+    #Create of StudentClass a Instance
+    setUpStudents
+
+    #Show of an Instance of the StudentClass
+    printStudents
+  end
+end
+
+#Create of Studentclass a Instance
+student_book = StudentBook.new
+
+#Show of an Instance of the Studentclass
+student_book.listAllStudents
 
